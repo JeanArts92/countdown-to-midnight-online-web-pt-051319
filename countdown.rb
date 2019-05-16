5cfd4b7c36fa3dd99bb5c39f2cf67f3fc9b322a6
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-05-16 20:31:40 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,7 +4,7 @@ def countdown(number)
   counter = 12
   while counter < 13
   puts "#{counter} SECOND(S)!"
-  break if counter < 1
+  break if counter < 2
   counter -= 1 
   end
   "HAPPY NEW YEAR!"
